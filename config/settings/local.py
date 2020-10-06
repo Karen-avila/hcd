34d2b3a147a7109e615812ee1b1124fffe256044
gcastellan0s <g@gustavo-castellanos.com>
2020-10-06 15:32:32 -0500

    Frontend: getDirectory

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="wrI9MLLFrGLMxZ7blYgqFlp8OlyetWoykOzGHM0jEFEXDkI26LHClUxoD7KQrgkZ",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------

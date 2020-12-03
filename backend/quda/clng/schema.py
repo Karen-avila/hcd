987f9222f27821de3ac6bbddc97c6be732889c0f
Gustavo Castellanos <xyz@Gustavos-MacBook-Pro.local>
2020-12-03 15:22:26 -0600

    Backend: Agregando cleaning

--- backend/quda/clng/schema.py
+++ backend/quda/clng/schema.py
@@ -6,11 +6,44 @@ from .forms import *
 #################################################################
 #########   TYPES or NODES   ####################################
 #################################################################
-class CleaningNode(DjangoObjectType):
+class CleaningFileNode(BaseNode):
+    getStatus = graphene.String(
+        source="getStatus",
+        description=""
+    )
     class Meta:
-        model = Cleaning
+        model = CleaningFile
+        filter_fields = {
+            'id': ['exact',],
+        }
+        description = ""
+        interfaces = (graphene.relay.Node,)
+        connection_class = ConnectionBase
+
 
 ###############################################
+class CleaningNode(BaseNode):
+    getCleaningFiles = graphene.List(CleaningFileNode,
+        source="getCleaningFiles",
+        description=""
+    )
+    getLenCleaningFiles = graphene.Int(
+        source="getLenCleaningFiles",
+        description=""
+    )
+    getStatus = graphene.String(
+        source="getStatus",
+        description=""
+    )
+    class Meta:
+        model = Cleaning
+        filter_fields = {
+            'id': ['exact', 'icontains', 'istartswith'],
+            'user__username': ['exact', 'icontains'],
+        }
+        interfaces = (graphene.relay.Node,)
+        connection_class = ConnectionBase
+
 
 #################################################################
 #########   QUERYS   ############################################
